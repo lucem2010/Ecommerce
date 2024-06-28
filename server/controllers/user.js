d57dd2a6f6ccf6625b3f19fcbec650e9c5bb2558
luc tien <lucd7464@gmail.com>
2024-06-28 11:15:59 +0700

    #2

---
+++ server/controllers/user.js
@@ -0,0 +1,26 @@
+const User = require('../models/user')
+const asyncHandler = require('express-async-handler')
+
+const register = asyncHandler(async (req, res) => {
+    const { email, password, firstname, lastname } = req.body
+    if (!email || !password || !lastname || !firstname)
+        return res.status(400).json({
+            sucess: false,
+            mes: 'Missing inputs'
+        })
+
+    const user = await User.findOne({ email })
+    if (user) throw new Error('User has existed')
+    else {
+        const newUser = await User.create(req.body)
+        return res.status(200).json({
+            sucess: newUser ? true : false,
+            mes: newUser ? 'Register is successfully. Please go login~' : 'Something went wrong'
+        })
+    }
+})
+
+module.exports = {
+    register,
+   
+}
--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 require('dotenv').config()
-
+const dbConnect = require('./config/dbconnect')
+const initRoutes = require('./routes')
 
 const app = express()
-const port = process.env.PORT || 8888
+const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+dbConnect()
+initRoutes(app)
 
 app.listen(port, () => {
     console.log('Server running on the port: ' + port);
